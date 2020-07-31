a192a9a12d5265485ec441d0975f4213c34c4abd
user11681 <username11681@gmail.com>
2020-07-31 03:17:13 +0100

    initial commit

---
+++ src/main/java/user11681/bason/Bason.java
@@ -0,0 +1,12 @@
+package user11681.bason;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bason {
+    @Override
+    public void onInitialize() {
+
+    }
+
+    public static void
+}
---
+++ src/main/java/user11681/bason/client/BasonClient.java
@@ -0,0 +1,9 @@
+package user11681.bason.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BasonClient {
+    public static void init() {}
+}
